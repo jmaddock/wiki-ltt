d95adcd7c8ef942642e1cb8ff414fb27b30161c9
Jim Maddock <skimaddster@gmail.com>
2020-08-25 09:49:20 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var d3 = require("d3");
+var app = express();
+
+app.use(express.static('static'));
+
+app.get('/*', function (req, res) {
+    res.sendFile( __dirname + "/" + "zoomable_knowledge_graph.html" );
+})
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+})
